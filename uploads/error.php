f9177908a66e63c5609067b52744c68ccbec921b
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2022-01-01 11:13:13 +0545

    removed php

--- uploads/error.php
+++
@@ -1,8 +0,0 @@
-<?php
-$name=$_GET['n'];
-$myfile = fopen("error.html", "a") or die("Unable to open file!");
-date_default_timezone_set("Asia/Kathmandu");
-$txt = $name . " = " . date("Y/m/d") . " on " . date("l") . " at " . date("h:i:s a") . ",\n <br> <br>\n";
-fwrite($myfile, $txt);
-fclose($myfile);
-?>
