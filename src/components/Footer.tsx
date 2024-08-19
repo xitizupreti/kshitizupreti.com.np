d4d420468708d7fe5094e1dd4e538d215cbdca0d
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2024-08-19 13:37:55 +0545

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,21 @@
 export default function Footer() {
   return (
     <div className=" bg-[#1f1f1f] h-[120px] p-5">
-      <div className=" p-3 w-[300px] ml-40 flex justify-around ">
-        <a className="text-[#726a61] hover:text-white" href="#">
+      <div className=" p-3 w-[70%] md:w-[30%] ml-[10%] mb-6  flex justify-between ">
+        <a className="text-[#726a61] hover:text-white" href="/#">
           Home
         </a>
-        <a className="text-[#726a61] hover:text-white" href="#about">
+        <a className="text-[#726a61] hover:text-white" href="/#about">
           About
         </a>
-        <a className="text-[#726a61] hover:text-white" href="#blog">
+        <a className="text-[#726a61] hover:text-white" href="/#blog">
           Blog
         </a>
-        <a className="text-[#726a61] hover:text-white" href="#">
+        <a className="text-[#726a61] hover:text-white" href="/contact#contact">
           Contacts
         </a>
       </div>
-      <p className="cursor-pointer float-right text-[#726a61] mr-[65px] hover:text-white">
+      <p className="cursor-pointer float-right text-[#726a61] mr-[10%] hover:text-white">
         All Rights Reserved. © 2023 (by XiTiZ)
       </p>
     </div>
