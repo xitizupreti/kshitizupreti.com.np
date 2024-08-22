606d0e9f770fb5774082f616addbc68624622c52
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2024-08-22 12:41:02 +0545

    update package

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
       />
 
       <div className="absolute inset-0 bg-black bg-opacity-50 z-1"></div>
-      <div className="absolute inset-0 flex flex-col items-center justify-center text-white z-10">
+      <div className="absolute mt-20 md:mt-0 inset-0 flex flex-col items-center justify-center text-white z-10">
         <h1 className="text-3xl font-bold ">
           <span className="dynamic-text">Hello I&apos;m Kshitiz Upreti</span>
         </h1>
